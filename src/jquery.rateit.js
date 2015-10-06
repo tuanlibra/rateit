d5af7fa418f7682ccc1d7261372cf20d022d9c6e
Tuan Jihoo <kennythanhtuan@gmail.com>
2015-10-06 16:55:56 +0700

    update

--- src/jquery.rateit.js
+++ src/jquery.rateit.js
@@ -302,7 +302,7 @@
     };
 
     //some default values.
-    $.fn.rateit.defaults = { min: 0, max: 5, step: 0.5, starwidth: 22, starheight: 17, readonly: false, resetable: true, ispreset: false};
+    $.fn.rateit.defaults = { min: 0, max: 5, step: 0.5, starwidth: 16, starheight: 16, readonly: false, resetable: true, ispreset: false};
 
     //invoke it on all .rateit elements. This could be removed if not wanted.
     //$(function () { $('div.rateit, span.rateit').rateit(); });
